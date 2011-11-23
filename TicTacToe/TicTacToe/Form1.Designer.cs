42a75377d4360d6993abb7a942c09eb6f8771520
Haflidi Orn Olafsson <flatmundur@gmail.com>
2011-11-23 12:36:49 +0000

    X og O koma thegar smellt er a reitina

--- TicTacToe/TicTacToe/Form1.Designer.cs
+++ TicTacToe/TicTacToe/Form1.Designer.cs
@@ -32,6 +32,15 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.picHorizontalLine = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.TopLeft = new System.Windows.Forms.Label();
+            this.TopCenter = new System.Windows.Forms.Label();
+            this.TopRight = new System.Windows.Forms.Label();
+            this.MiddleLeft = new System.Windows.Forms.Label();
+            this.MiddleCenter = new System.Windows.Forms.Label();
+            this.MiddleRight = new System.Windows.Forms.Label();
+            this.BottomLeft = new System.Windows.Forms.Label();
+            this.BottomCenter = new System.Windows.Forms.Label();
+            this.BottomRight = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picHorizontalLine)).BeginInit();
@@ -78,11 +87,110 @@
             this.pictureBox3.TabIndex = 3;
             this.pictureBox3.TabStop = false;
             // 
+            // TopLeft
+            // 
+            this.TopLeft.AutoSize = true;
+            this.TopLeft.Location = new System.Drawing.Point(44, 51);
+            this.TopLeft.Name = "TopLeft";
+            this.TopLeft.Size = new System.Drawing.Size(37, 13);
+            this.TopLeft.TabIndex = 4;
+            this.TopLeft.Text = "Select";
+            this.TopLeft.Click += new System.EventHandler(this.TopLeft_Click);
+            // 
+            // TopCenter
+            // 
+            this.TopCenter.AutoSize = true;
+            this.TopCenter.Location = new System.Drawing.Point(170, 51);
+            this.TopCenter.Name = "TopCenter";
+            this.TopCenter.Size = new System.Drawing.Size(37, 13);
+            this.TopCenter.TabIndex = 5;
+            this.TopCenter.Text = "Select";
+            this.TopCenter.Click += new System.EventHandler(this.TopCenter_Click);
+            // 
+            // TopRight
+            // 
+            this.TopRight.AutoSize = true;
+            this.TopRight.Location = new System.Drawing.Point(292, 51);
+            this.TopRight.Name = "TopRight";
+            this.TopRight.Size = new System.Drawing.Size(37, 13);
+            this.TopRight.TabIndex = 6;
+            this.TopRight.Text = "Select";
+            this.TopRight.Click += new System.EventHandler(this.TopRight_Click);
+            // 
+            // MiddleLeft
+            // 
+            this.MiddleLeft.AutoSize = true;
+            this.MiddleLeft.Location = new System.Drawing.Point(47, 158);
+            this.MiddleLeft.Name = "MiddleLeft";
+            this.MiddleLeft.Size = new System.Drawing.Size(37, 13);
+            this.MiddleLeft.TabIndex = 7;
+            this.MiddleLeft.Text = "Select";
+            this.MiddleLeft.Click += new System.EventHandler(this.MiddleLeft_Click);
+            // 
+            // MiddleCenter
+            // 
+            this.MiddleCenter.AutoSize = true;
+            this.MiddleCenter.Location = new System.Drawing.Point(173, 158);
+            this.MiddleCenter.Name = "MiddleCenter";
+            this.MiddleCenter.Size = new System.Drawing.Size(37, 13);
+            this.MiddleCenter.TabIndex = 8;
+            this.MiddleCenter.Text = "Select";
+            this.MiddleCenter.Click += new System.EventHandler(this.MiddleCenter_Click);
+            // 
+            // MiddleRight
+            // 
+            this.MiddleRight.AutoSize = true;
+            this.MiddleRight.Location = new System.Drawing.Point(295, 157);
+            this.MiddleRight.Name = "MiddleRight";
+            this.MiddleRight.Size = new System.Drawing.Size(37, 13);
+            this.MiddleRight.TabIndex = 9;
+            this.MiddleRight.Text = "Select";
+            this.MiddleRight.Click += new System.EventHandler(this.MiddleRight_Click);
+            // 
+            // BottomLeft
+            // 
+            this.BottomLeft.AutoSize = true;
+            this.BottomLeft.Location = new System.Drawing.Point(47, 259);
+            this.BottomLeft.Name = "BottomLeft";
+            this.BottomLeft.Size = new System.Drawing.Size(37, 13);
+            this.BottomLeft.TabIndex = 10;
+            this.BottomLeft.Text = "Select";
+            this.BottomLeft.Click += new System.EventHandler(this.BottomLeft_Click);
+            // 
+            // BottomCenter
+            // 
+            this.BottomCenter.AutoSize = true;
+            this.BottomCenter.Location = new System.Drawing.Point(173, 259);
+            this.BottomCenter.Name = "BottomCenter";
+            this.BottomCenter.Size = new System.Drawing.Size(37, 13);
+            this.BottomCenter.TabIndex = 11;
+            this.BottomCenter.Text = "Select";
+            this.BottomCenter.Click += new System.EventHandler(this.BottomCenter_Click);
+            // 
+            // BottomRight
+            // 
+            this.BottomRight.AutoSize = true;
+            this.BottomRight.Location = new System.Drawing.Point(295, 259);
+            this.BottomRight.Name = "BottomRight";
+            this.BottomRight.Size = new System.Drawing.Size(37, 13);
+            this.BottomRight.TabIndex = 12;
+            this.BottomRight.Text = "Select";
+            this.BottomRight.Click += new System.EventHandler(this.BottomRight_Click);
+            // 
             // TicTacToeApplicationWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(364, 334);
+            this.Controls.Add(this.BottomRight);
+            this.Controls.Add(this.BottomCenter);
+            this.Controls.Add(this.BottomLeft);
+            this.Controls.Add(this.MiddleRight);
+            this.Controls.Add(this.MiddleCenter);
+            this.Controls.Add(this.MiddleLeft);
+            this.Controls.Add(this.TopRight);
+            this.Controls.Add(this.TopCenter);
+            this.Controls.Add(this.TopLeft);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
@@ -96,6 +204,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.picHorizontalLine)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -105,6 +214,15 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label TopLeft;
+        private System.Windows.Forms.Label TopCenter;
+        private System.Windows.Forms.Label TopRight;
+        private System.Windows.Forms.Label MiddleLeft;
+        private System.Windows.Forms.Label MiddleCenter;
+        private System.Windows.Forms.Label MiddleRight;
+        private System.Windows.Forms.Label BottomLeft;
+        private System.Windows.Forms.Label BottomCenter;
+        private System.Windows.Forms.Label BottomRight;
     }
 }
 
--- TicTacToe/TicTacToe/Form1.cs
+++ TicTacToe/TicTacToe/Form1.cs
@@ -11,9 +11,75 @@ namespace TicTacToe
 {
     public partial class TicTacToeApplicationWindow : Form
     {
+        public static String player1 = "X";
+        public static String player2 = "O";
+        public String currentPlayer = player1;
         public TicTacToeApplicationWindow()
         {
             InitializeComponent();
+        }
+
+        public void ChangePlayers()
+        {
+            if (currentPlayer == player1)
+                currentPlayer = player2;
+            else
+                currentPlayer = player1;
+        }
+
+        public void MarkField(Label sender)
+        {
+            if (sender.Text == "Select")
+            {
+                sender.Text = currentPlayer;
+                sender.Refresh();
+                ChangePlayers();
+            }
+        }
+
+        private void TopLeft_Click(object sender, EventArgs e)
+        {
+            MarkField(TopLeft);
+        }
+
+        private void TopCenter_Click(object sender, EventArgs e)
+        {
+            MarkField(TopCenter);
+        }
+
+        private void TopRight_Click(object sender, EventArgs e)
+        {
+            MarkField(TopRight);
+        }
+
+        private void MiddleLeft_Click(object sender, EventArgs e)
+        {
+            MarkField(MiddleLeft);
+        }
+
+        private void MiddleCenter_Click(object sender, EventArgs e)
+        {
+            MarkField(MiddleCenter);
+        }
+
+        private void MiddleRight_Click(object sender, EventArgs e)
+        {
+            MarkField(MiddleRight);
+        }
+
+        private void BottomLeft_Click(object sender, EventArgs e)
+        {
+            MarkField(BottomLeft);
+        }
+
+        private void BottomCenter_Click(object sender, EventArgs e)
+        {
+            MarkField(BottomCenter);
+        }
+
+        private void BottomRight_Click(object sender, EventArgs e)
+        {
+            MarkField(BottomRight);
         }
     }
 }
