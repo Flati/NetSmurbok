499e239c10fc257719123d6e196bdff64aa271ab
Haflidi Orn Olafsson <flatmundur@gmail.com>
2011-11-23 13:27:36 +0000

    Matrix baett vid fyrir current stodu

--- TicTacToe/TicTacToe/Form1.cs
+++ TicTacToe/TicTacToe/Form1.cs
@@ -14,9 +14,25 @@ namespace TicTacToe
         public static String player1 = "X";
         public static String player2 = "O";
         public String currentPlayer = player1;
+        public static List<List<string>> matrix = new List<List<string>>();
         public TicTacToeApplicationWindow()
         {
             InitializeComponent();
+            for (int i = 0; i < 3; i++)
+            {
+                matrix.Add(new List<string>());
+                for (int j = 0; j < 3; j++)
+                {
+                    matrix[i].Add("E");
+                }
+            }
+        }
+
+        public bool isAvailable(Label field)
+        {
+            if (field.Text == "Select")
+                return true;
+            return false;
         }
 
         public void ChangePlayers()
@@ -29,57 +45,95 @@ namespace TicTacToe
 
         public void MarkField(Label sender)
         {
-            if (sender.Text == "Select")
-            {
-                sender.Text = currentPlayer;
-                sender.Refresh();
-                ChangePlayers();
-            }
+            sender.Text = currentPlayer;
+            sender.Refresh();
+        }
+
+        private void MarkMatrix(int x, int y)
+        {
+            matrix[x][y] = currentPlayer;
+            ChangePlayers();
         }
 
         private void TopLeft_Click(object sender, EventArgs e)
         {
-            MarkField(TopLeft);
+            if (isAvailable(TopLeft))
+            {
+                MarkField(TopLeft);
+                MarkMatrix(0, 0);
+            }
         }
 
         private void TopCenter_Click(object sender, EventArgs e)
         {
-            MarkField(TopCenter);
+            if (isAvailable(TopCenter))
+            {
+                MarkField(TopCenter);
+                MarkMatrix(0, 1);
+            }
         }
 
         private void TopRight_Click(object sender, EventArgs e)
         {
-            MarkField(TopRight);
+            if (isAvailable(TopRight))
+            {
+                MarkField(TopRight);
+                MarkMatrix(0, 2);
+            }
         }
 
         private void MiddleLeft_Click(object sender, EventArgs e)
         {
-            MarkField(MiddleLeft);
+            if (isAvailable(MiddleLeft))
+            {
+                MarkField(MiddleLeft);
+                MarkMatrix(1, 0);
+            }
         }
 
         private void MiddleCenter_Click(object sender, EventArgs e)
         {
-            MarkField(MiddleCenter);
+            if (isAvailable(MiddleCenter))
+            {
+                MarkField(MiddleCenter);
+                MarkMatrix(1, 1);
+            }
         }
 
         private void MiddleRight_Click(object sender, EventArgs e)
         {
-            MarkField(MiddleRight);
+            if (isAvailable(MiddleRight))
+            {
+                MarkField(MiddleRight);
+                MarkMatrix(1, 2);
+            }
         }
 
         private void BottomLeft_Click(object sender, EventArgs e)
         {
-            MarkField(BottomLeft);
+            if (isAvailable(BottomLeft))
+            {
+                MarkField(BottomLeft);
+                MarkMatrix(2, 0);
+            }
         }
 
         private void BottomCenter_Click(object sender, EventArgs e)
         {
-            MarkField(BottomCenter);
+            if (isAvailable(BottomCenter))
+            {
+                MarkField(BottomCenter);
+                MarkMatrix(2, 1);
+            }
         }
 
         private void BottomRight_Click(object sender, EventArgs e)
         {
-            MarkField(BottomRight);
+            if (isAvailable(BottomRight))
+            {
+                MarkField(BottomRight);
+                MarkMatrix(2, 2);
+            }
         }
     }
 }
