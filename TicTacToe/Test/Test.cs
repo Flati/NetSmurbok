f589009df0ee6717d1b9a2d249c4f46f72dfdf7e
Petur Haukur Johannesson <pesinn@gmail.com>
2011-11-23 21:50:35 +0000

    Unit test is on

---
+++ TicTacToe/Test/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace test
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void testSomething()
+        {
+            int a = 0;
+            Assert.AreEqual(a, a, "Whupwhup");
+        }
+    }
+}
